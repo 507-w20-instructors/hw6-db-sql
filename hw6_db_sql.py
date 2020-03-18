908847aff4796119fb255aaf514909778b8b934d
Kangning Chen <knchen@umich.edu>
2020-03-18 11:26:20 -0400

    Add starter code

---
+++ hw6_db_sql.py
@@ -0,0 +1,248 @@
+'''
+Name:
+Uniqname:
+'''
+
+import sqlite3 
+
+def question0():
+    ''' Constructs and executes SQL query to retrieve
+    all fields from the Region table
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    pass
+    #TODO Implement function
+
+    connection = sqlite3.connect("Northwind_small.sqlite")
+    cursor = connection.cursor()
+    query = "SELECT * FROM Region"
+    result = cursor.execute(query).fetchall()
+    print(result)
+
+def question1():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question2():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question3():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question4():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function 
+    pass
+
+def question5():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question6():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question7():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question8():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question9():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question10():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question11():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+def question12():
+    ''' Constructs and executes SQL query to retrieve
+    data based on requirements
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    list
+        a list of tuples that represent the query result
+    '''
+    #TODO Implement function
+    pass
+
+
+
+#################################################################
+########################  ECs start here  #######################
+#################################################################
+
+# EC1
+def print_query_result(raw_query_result):
+    ''' Pretty prints raw query result
+    
+    Parameters
+    ----------
+    list 
+        a list of tuples that represent raw query result
+    
+    Returns
+    -------
+    None
+    '''
+    #TODO Implement function
+    pass
+
+# EC2
+if __name__ == "__main__":
+    #TODO Implement interactive program here
+    pass
+
